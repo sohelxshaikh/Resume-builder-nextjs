a3ba20fb671a27cd661bc622e6119d2af0e7587a
Sohel Shaikh <84074486+sohelxshaikh@users.noreply.github.com>
2025-09-26 22:54:25 +0530

    Update Person.tsx

--- src/modules/home/components/Person.tsx
+++ src/modules/home/components/Person.tsx
@@ -1,7 +1,26 @@
 import Image from 'next/image';
 
+
 const persons = [
   {
+    name: 'Rohaan Shaikh',
+    role: '',
+    avatar: 'https://avatars.githubusercontent.com/u/84074486?v=4',
+    github: 'https://github.com/sohelxshaikh',
+    linkedin: 'https://www.linkedin.com/in/sohelxshaikh/',
+  },
+  {
+    name: 'Vivek Yadav',
+    role: 'Frontend developer',
+    avatar: 'https://avatars.githubusercontent.com/u/169388702?v=4',
+    github: 'https://github.com/vivyadav',
+    linkedin: 'https://www.linkedin.com/in/',
+  },
+  {
+    name: 'Vivek Saxena',
+    role: 'Product designer, half developer',
+    avatar: 'https://avatars.githubusercontent.com/u/107567647?v=4',
+    github: 'https://github.com/viveksaxena1',
     name: 'Rohaan Shaikh',
    
     avatar: 'https://avatars.githubusercontent.com/u/8v=4',
@@ -9,6 +28,9 @@ const persons = [
     linkedin: 'https://www.linkedin.com/in/',
   },
   {
+    name: 'Sumit Prajapat',
+    role: 'Frontend developer',
+    avatar: 'https://avatars.githubusercontent.com/u/60530010?v=4',
     name: 'Rohit Mobar',
    
     avatar: 'https://avatars.githubusercontent.com/u/?v=4',
